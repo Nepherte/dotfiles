ab214974ce720ab1516c8b124ed5ad0ad4ecc9a6
Bart Verhoeven <bartverhoeven@live.com>
2023-01-11 00:36:32 +0800

    Initial changes from branch 'master' into 'khepri'.

---
+++ applications.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env zsh
+
+# This is a template which illustrates how the included scripts can be used
+# to install applications. For real-world examples, check the various branches.
+
+DOTFILES="${0:A:h}"
+
+. "${DOTFILES}/helpers/echos.sh"
+. "${DOTFILES}/helpers/brew.sh"
+. "${DOTFILES}/helpers/cmd.sh"
+. "${DOTFILES}/helpers/git.sh"
+. "${DOTFILES}/helpers/mac.sh"
+. "${DOTFILES}/helpers/os.sh"
+
+header "Applications"
+ask_for_sudo
+
+##
+## Install Prerequisites
+##
+title "Prerequisites"
+install_command_line_tools
+install_brew
+
+if on_arm; then
+  install_rosetta
+fi
+
+##
+## Repositories
+##
+title "Repositories"
+
+comment "Formulae"
+tap "homebrew/core"
+tap "homebrew/services"
+
+comment "Casks"
+tap "homebrew/cask"
+
+##
+## Install packages
+##
+title "Packages"
+
+# Command Line Tools
+comment "Command Line Tools"
+formula "coreutils"
+
+# Productivity
+comment "Productivity"
+cask "alfred"
---
+++ configurations.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# This is a template which illustrates how the included scripts can be used
+# to symlink dotfiles. For real-world examples, check the various branches.
+
+DOTFILES="${0:A:h}"
+
+. "${DOTFILES}/helpers/echos.sh"
+. "${DOTFILES}/helpers/config.sh"
+
+header "Configurations"
+
+##
+## zsh
+##
+title "zsh"
+config zsh/zshrc .zshrc
