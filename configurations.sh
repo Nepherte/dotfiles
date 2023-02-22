98754fcc6daea969db2d8e2f0b30ead0eb6f8631
Bart Verhoeven <bartverhoeven@live.com>
2023-02-22 13:07:17 +0800

    Initial configuration files for 'napith'.

--- configurations.sh
+++ configurations.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env zsh
 
-# This is a template which illustrates how the included scripts can be used
-# to symlink dotfiles. For real-world examples, check the various branches.
-
 DOTFILES="${0:A:h}"
 
 . "${DOTFILES}/helpers/echos.sh"
@@ -11,7 +8,45 @@ DOTFILES="${0:A:h}"
 header "Configurations"
 
 ##
+## dircolors
+##
+title "dircolors"
+config dircolors/dircolors .dircolors
+
+##
+## git
+##
+title "git"
+config git/gitconfig .gitconfig
+
+##
+## gpg
+##
+title "gpg"
+config gpg/gpg.conf .gnupg/gpg.conf
+config gpg/gpg-agent.conf .gnupg/gpg-agent.conf
+config gpg/scdaemon.conf .gnupg/scdaemon.conf
+
+##
+## ssh
+##
+title "ssh"
+config ssh/config .ssh/config
+config ssh/id_anubis.pub .ssh/id_anubis.pub
+config ssh/id_github.pub .ssh/id_github.pub
+config ssh/id_horus.pub .ssh/id_horus.pub
+config ssh/id_isis.pub .ssh/id_isis.pub
+config ssh/id_osiris.pub .ssh/id_osiris.pub
+config ssh/id_unifi.pub .ssh/id_unifi.pub
+
+##
 ## zsh
 ##
 title "zsh"
 config zsh/zshrc .zshrc
+config zsh/zprofile .zprofile
+
+config zsh/aliases .zsh/aliases
+config zsh/exports .zsh/exports
+config zsh/options .zsh/options
+config zsh/theme .zsh/theme
