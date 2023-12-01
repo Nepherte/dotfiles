13e27d0cf331fbf6bf29fcade5df5181a78499ad
Bart Verhoeven <bartverhoeven@live.com>
2023-12-01 14:14:07 +0800

    Add public key for luciad.

--- configurations.sh
+++ configurations.sh
@@ -34,6 +34,7 @@ config ssh/id_anubis.pub .ssh/id_anubis.pub
 config ssh/id_github.pub .ssh/id_github.pub
 config ssh/id_horus.pub .ssh/id_horus.pub
 config ssh/id_isis.pub .ssh/id_isis.pub
+config ssh/id_luciad.pub .ssh/id_luciad.pub
 config ssh/id_osiris.pub .ssh/id_osiris.pub
 config ssh/id_unifi.pub .ssh/id_unifi.pub
 
