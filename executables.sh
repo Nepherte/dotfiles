233c62afbcdb53ea3b935525c7741fd6d7f5fc52
Bart Verhoeven <nepherte@pm.me>
2024-04-23 20:21:24 +0800

    Remove template headers on device-specific branch.

--- executables.sh
+++ executables.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env zsh
 
-# This is a template which illustrates how the included scripts can be used
-# to symlink executables. For real-world examples, check the various branches.
-
 DOTFILES="${0:A:h}"
 
 . "${DOTFILES}/helpers/echos.sh"
