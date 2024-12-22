bf338240f3bac8575d6be9eed8d38767c378b3d7
Bart Verhoeven <nepherte@pm.me>
2024-12-22 11:55:11 +0800

    Add luciad public ssh key.

--- configurations.sh
+++ configurations.sh
@@ -38,6 +38,7 @@ config ssh/config .ssh/config
 config ssh/id_anubis.pub .ssh/id_anubis.pub
 config ssh/id_github.pub .ssh/id_github.pub
 config ssh/id_horus.pub .ssh/id_horus.pub
+config ssh/id_luciad.pub .ssh/id_luciad.pub
 config ssh/id_osiris.pub .ssh/id_osiris.pub
 config ssh/id_unifi.pub .ssh/id_unifi.pub
 
