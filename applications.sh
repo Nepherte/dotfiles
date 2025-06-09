4b07782c023c26b8905df6249b48e54ca3d769fe
Bart Verhoeven <verhoevenbart@icloud.com>
2025-06-09 15:28:31 +0800

    Import initial configs.

--- applications.sh
+++ applications.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env zsh
 
-# This is a template which illustrates how the included scripts can be used
-# to install applications. For real-world examples, check the various branches.
-
 DOTFILES="${0:A:h}"
 
 . "${DOTFILES}/helpers/echos.sh"
 . "${DOTFILES}/helpers/brew.sh"
 . "${DOTFILES}/helpers/cmd.sh"
+. "${DOTFILES}/helpers/git.sh"
 . "${DOTFILES}/helpers/mac.sh"
 
 header "Applications"
@@ -25,7 +23,82 @@ install_brew
 ##
 section "Repositories"
 
+comment "Oh-my-zsh"
+git "https://github.com/ohmyzsh/ohmyzsh.git" "/opt/oh-my-zsh"
+git "https://github.com/qoomon/zsh-lazyload.git" "/opt/oh-my-zsh/custom/plugins/zsh-lazyload"
+git "https://github.com/romkatv/powerlevel10k.git" "/opt/oh-my-zsh/custom/themes/powerlevel10k"
+
 ##
 ## Packages
 ##
 section "Packages"
+
+# Command Line Tools
+comment "Command Line Tools"
+formula "coreutils"
+formula "tree"
+
+# Shells
+comment "Shells"
+formula "zsh"
+formula "zsh-completions"
+
+# Productivity
+comment "Productivity"
+cask "alfred"
+cask "firefox"
+cask "iterm2"
+cask "rectangle-pro"
+
+# Dependency Management
+comment "Dependency Management"
+formula "jenv"
+formula "volta"
+formula "pyenv"
+
+# Java
+comment "Java"
+cask "microsoft-openjdk@11"
+cask "microsoft-openjdk@17"
+cask "microsoft-openjdk@21"
+
+# Office
+comment "Office"
+cask "microsoft-word"
+cask "microsoft-excel"
+cask "microsoft-powerpoint"
+cask "microsoft-outlook"
+cask "microsoft-teams"
+
+# Communication
+comment "Communication"
+cask "whatsapp"
+
+# Peripherals
+comment "Peripherals"
+cask "logi-options+"
+
+# Developer Tools
+comment "Developer Tools"
+formula "git"
+cask "docker"
+cask "httpie"
+
+# Editors
+comment "Editors"
+cask "intellij-idea"
+
+# Multimedia
+comment "Multimedia"
+cask "roon"
+cask "tidal"
+
+# Networking
+comment "Networking"
+cask "transmission"
+cask "transmit"
+
+# Security
+comment "Security"
+cask "1password"
+cask "yubico-authenticator"
--- configurations.sh
+++ configurations.sh
@@ -1,11 +1,55 @@
 #!/usr/bin/env zsh
 
-# This is a template which illustrates how the included scripts can be used
-# to symlink config files. For real-world examples, check the various branches.
-
 DOTFILES="${0:A:h}"
 
 . "${DOTFILES}/helpers/echos.sh"
 . "${DOTFILES}/helpers/config.sh"
 
 header "Configurations"
+
+##
+## dircolors
+##
+section "dircolors"
+config dircolors/dircolors .dircolors
+
+##
+## git
+##
+section "git"
+config git/gitconfig .gitconfig
+config git/allowed_signers .git/allowed_signers
+
+config git/includes/1password.inc .git/includes/1password.inc
+config git/includes/intellij.inc .git/includes/intellij.inc
+
+##
+## maven
+##
+section "maven"
+config mvn/settings.xml .m2/settings.xml
+
+##
+## ssh
+##
+section "ssh"
+config ssh/config .ssh/config
+
+##
+## vim
+##
+section "vim"
+config vim/vimrc .vimrc
+
+##
+## zsh
+##
+section "zsh"
+config zsh/zshrc .zshrc
+config zsh/zprofile .zprofile
+
+config zsh/aliases .zsh/aliases
+config zsh/exports .zsh/exports
+config zsh/options .zsh/options
+config zsh/plugins .zsh/plugins
+config zsh/theme .zsh/theme
